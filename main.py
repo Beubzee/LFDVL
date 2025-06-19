04e87d82c257bcc4bca0358a1b32d9d6c9664f7a
Beubzee <130854891+Beubzee@users.noreply.github.com>
2025-06-19 16:27:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 import tweepy
 from datetime import datetime
 import os
