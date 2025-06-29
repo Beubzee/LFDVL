73401efdc374b501f3b67e3f3bbe55f8862a0e59
Beubzee <130854891+Beubzee@users.noreply.github.com>
2025-06-29 22:45:02 +0200

    Ajout téléchargement musique

--- main.py
+++ main.py
@@ -9,28 +9,38 @@ print('App créée.')
 
 @app.route("/robots.txt")
 def robots():
-    print('---------------------------------------------------')
+    print('\n---------------------------------------------------')
     print(f'Requête robots.txt - {str(datetime.now())}')
     return send_from_directory("static", "robots.txt")
 
 @app.route("/sitemap.xml")
 def sitemap():
-    print('---------------------------------------------------')
+    print('\n---------------------------------------------------')
     print(f'Requête sitemap.xml - {str(datetime.now())}')
     return send_from_directory("static", "sitemap.xml")
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def accueil():
-    print('---------------------------------------------------')
+    print('\n---------------------------------------------------')
     print(f'Requête page accueil - {str(datetime.now())}')
+    if request.method == 'POST':
+        donnee = request.form
+        son_dl = donnee.get('son')
+        print(f'Formulaire entrant : {donnee}')
+        if son_dl == 'HEAD TO THE ROAD':
+            print('Métode POST -- téléchargment HEAD TO THE ROAD')
+            return send_from_directory('static/music', 'HEAD TO THE ROAD - Les Fous Du Van Lent.mp3', as_attachment=True)
+        elif son_dl == 'Wakey Wakey':
+            print('Métode POST -- téléchargment Wakey Wakey')
+            return send_from_directory('static/music', 'Wakey Wakey - Les Fous Du Van Lent.mp3', as_attachment=True)
     return render_template('Accueil.html')
 
 @app.route('/affiche_road_trip')
 def affiche_road_trip():
-    print('---------------------------------------------------')
-    print('Requête page affiche')
+    print('\n---------------------------------------------------')
+    print(f'Requête page affiche - {str(datetime.now())}')
     return render_template('Affiche_road_trip.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
